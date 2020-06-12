72b50aaeda456bdd871dd5e6eaeb5ae19bb7f382
Ismael Cabañas García <ismael.cabanas@adevinta.com>
2020-06-12 23:23:58 +0200

    First commit

---
+++ src/test/java/org/ismael/cabanas/garcia/springboot/logging_consumer/LoggingConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ismael.cabanas.garcia.springboot.logging_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
